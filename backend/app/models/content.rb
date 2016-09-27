7901f1e4a1055c4384cd4a47007b1212b52adb20
Simao Belchior <simaobelchior@gmail.com>
2016-09-27 08:15:51 +0100

    Adds pictures for contents

--- backend/app/models/content.rb
+++ backend/app/models/content.rb
@@ -17,6 +17,9 @@
 class Content < ApplicationRecord
   include Publishable
 
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
   has_many :participants
   has_many :users, through: :participants
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926225555) do
+ActiveRecord::Schema.define(version: 20160927071144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,12 @@ ActiveRecord::Schema.define(version: 20160926225555) do
     t.boolean  "is_published"
     t.integer  "position"
     t.string   "story_map_url"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "events", force: :cascade do |t|
