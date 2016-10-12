ce382b9871c3e19d5c31fe586e63c34ce8638735
Simao Belchior <simaobelchior@gmail.com>
2016-10-12 14:01:21 +0200

    Adds project_number for contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927071144) do
+ActiveRecord::Schema.define(version: 20161012115522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20160927071144) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.boolean  "is_featured"
+    t.integer  "project_number"
   end
 
   create_table "events", force: :cascade do |t|
@@ -116,7 +118,7 @@ ActiveRecord::Schema.define(version: 20160927071144) do
     t.string   "web_url"
     t.boolean  "active",                 default: false, null: false
     t.datetime "deactivated_at"
-    t.integer  "role",                   default: 0,     null: false
+    t.integer  "role",                   default: 0,     null: false, comment: "User role { contributor: 0, publisher: 1, admin: 2 }"
     t.datetime "locked_at"
     t.integer  "failed_attempts",        default: 0,     null: false
     t.string   "unlock_token"
@@ -126,6 +128,14 @@ ActiveRecord::Schema.define(version: 20160927071144) do
     t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "vacancies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.boolean  "is_published", default: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
 end
