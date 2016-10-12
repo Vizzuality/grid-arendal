08ca8ed3486f95d047b7b90681b3cf9a312bec33
Simao Belchior <simaobelchior@gmail.com>
2016-10-12 15:09:43 +0200

    Adds is_lead to participants join model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012124142) do
+ActiveRecord::Schema.define(version: 20161012130848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,8 +83,9 @@ ActiveRecord::Schema.define(version: 20161012124142) do
   create_table "participants", force: :cascade do |t|
     t.integer  "content_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "is_lead",    default: false
   end
 
   create_table "partners", force: :cascade do |t|
