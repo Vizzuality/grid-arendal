9664a7bf648fa7fb9fabb45145e8e73bc123ea83
Clara Linos <clara.linos@gmail.com>
2016-10-13 17:04:12 +0200

    js settings

---
+++ app/assets/javascripts/controllers/publications_controller.js
@@ -0,0 +1,19 @@
+(function(App) {
+
+  'use strict';
+
+  App.Controller = App.Controller || {};
+
+  App.Controller.Publications = App.Controller.Page.extend({
+
+    index: function(params) {
+      console.log('publications#index');
+    },
+
+    show: function(params) {
+      console.log('publications#show');
+    },
+
+  });
+
+})(this.App);
