f5c7947f87cc562d67f05bbc78b0b41a330bbe7d
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-10-14 13:19:54 +0200

    Allows to update photos (replace: image and meta) on album form as nested object

--- backend/app/services/flickr_service.rb
+++ backend/app/services/flickr_service.rb
@@ -26,10 +26,11 @@ module FlickrService
           album_photo_url = FlickRaw.url_o(flickr.photos.getInfo(photo_id: album_photo_id))
           album_photo_ids << album_photo_id
 
-          album_photo = MediaContent.create(album_photo_params.except(:_destroy, :main_photo_file))
-          album_photo.save
-          album_photo.set_photo(photo_id: album_photo_id, photo_url: album_photo_url)
-          AlbumRelation.create(photoset_id: media_content.id, album_photo_id: album_photo.id)
+          album_photo = MediaContent.new(album_photo_params.except(:_destroy, :main_photo_file))
+          if album_photo.save
+            album_photo.set_photo(photo_id: album_photo_id, photo_url: album_photo_url)
+            AlbumRelation.create(photoset_id: media_content.id, album_photo_id: album_photo.id)
+          end
 
           if main.present?
             @photoset = flickr.photosets.create(primary_photo_id: album_photo_id, title: title, description: description)
@@ -66,13 +67,48 @@ module FlickrService
 
       if photoset_id.present?
         if album_photos.present?
-          # main_photo_id  = flickr.upload_photo(main_photo_file.tempfile.path, title: title, description: description)
-          # main_photo_url = FlickRaw.url_o(flickr.photos.getInfo(photo_id: main_photo_id))
-          # flickr.photosets.editPhotos(photoset_id: photoset_id, primary_photo_id: main_photo_id, photo_ids: main_photo_id)
-          # media_content.update_album(main_photo_id: main_photo_id, main_photo_url: main_photo_url)
+          album_photos.values.each do |album_photo_params|
+            album_photo_file  = album_photo_params['photo_file']      if album_photo_params['photo_file'].present?
+            photo_title       = album_photo_params['title']           if album_photo_params['title'].present?
+            photo_description = album_photo_params['description']     if album_photo_params['description'].present?
+            main              = album_photo_params['main_photo_file'] if album_photo_params['main_photo_file'].present?
+            album_photo_id    = album_photo_params['id'].to_i         if album_photo_params['id'].present?
+
+            if album_photo_file.present? && album_photo_id.present?
+              if album_photo = MediaContent.find(album_photo_id)
+                album_photo_id = album_photo.photo_id
+                album_photo_id = flickr.replace_photo(photo_file.tempfile.path, photo_id: album_photo_id)
+                album_photo_url = FlickRaw.url_o(flickr.photos.getInfo(photo_id: album_photo_id))
+                flickr.photos.setMeta(photo_id: album_photo_id, title: photo_title, description: photo_description)
+                album_photo.update_photo(photo_id: album_photo_id, photo_url: album_photo_url)
+                album_photo.update(title: photo_title, description: photo_description)
+              end
+            elsif album_photo_file.present?
+              album_photo_id  = flickr.upload_photo(album_photo_file.tempfile.path, title: photo_title, description: photo_description)
+              album_photo_url = FlickRaw.url_o(flickr.photos.getInfo(photo_id: album_photo_id))
+              album_photo = MediaContent.find(media_content.album_photos.not_photos.pluck(:id).first)
+              if album_photo.present?
+                album_photo.set_photo(photo_id: album_photo_id, photo_url: album_photo_url)
+              end
+            else
+              if album_photo = MediaContent.find(album_photo_id)
+                album_photo_id  = album_photo.photo_id
+                album_photo_url = album_photo.photo_url
+                flickr.photos.setMeta(photo_id: album_photo_id, title: photo_title, description: photo_description)
+                album_photo.update(title: photo_title, description: photo_description)
+              end
+            end
+
+            if main.present?
+              media_content.update_album(main_photo_id: album_photo_id, main_photo_url: album_photo_url)
+            end
+          end
         end
         begin
+          album_photo_ids = media_content.reload.album_photos.map(&:photo_id)
+          album_photo_ids = album_photo_ids.join(',')
           flickr.photosets.editMeta(photoset_id: photoset_id, title: title, description: description)
+          flickr.photosets.editPhotos(photoset_id: photoset_id, primary_photo_id: media_content.main_photo_id, photo_ids: album_photo_ids)
         rescue FlickRaw::FailedResponse
           nil
         end
