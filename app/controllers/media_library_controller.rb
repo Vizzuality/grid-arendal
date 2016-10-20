aefb2fe2d68ba11521e4b416e99cab0d7a7e6fa7
pjosh <jorgegar@gmail.com>
2016-10-20 16:57:23 +0200

    Show media skeleton

--- app/controllers/media_library_controller.rb
+++ app/controllers/media_library_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class MediaLibraryController < ApplicationController
+  before_action :set_media, only: [:show]
 
   def index
     @publications = Publication.order(:title).published
@@ -7,5 +8,11 @@ class MediaLibraryController < ApplicationController
 
   def show
   end
+
+  private
+
+    def set_media
+      
+    end
 
 end
