41b8ac1bd2bcc5305d7bf55c23d0b747880981c2
pjosh <jorgegar@gmail.com>
2016-11-14 13:45:52 +0100

    Revert home redirect

--- backend/app/controllers/backend/admin_home_controller.rb
+++ backend/app/controllers/backend/admin_home_controller.rb
@@ -4,7 +4,6 @@ require_dependency 'backend/application_controller'
 module Backend
   class AdminHomeController < ::Backend::ApplicationController
     def index
-      redirect_to new_activity_path()
     end
   end
 end
