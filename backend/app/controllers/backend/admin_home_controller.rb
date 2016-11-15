6997687289fe316a10592c2d71aab9a5fd4dc3fc
pjosh <jorgegar@gmail.com>
2016-11-15 13:48:38 +0100

    Redirect when don't have session

--- backend/app/controllers/backend/admin_home_controller.rb
+++ backend/app/controllers/backend/admin_home_controller.rb
@@ -4,6 +4,9 @@ require_dependency 'backend/application_controller'
 module Backend
   class AdminHomeController < ::Backend::ApplicationController
     def index
+      if !user_signed_in?
+        redirect_to new_user_session_url
+      end
     end
   end
 end
