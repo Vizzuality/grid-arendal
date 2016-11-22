248a2f6a3c2592a462973b1b6e29ca2dacd88887
pjosh <jorgegar@gmail.com>
2016-11-22 13:34:41 +0100

    Form ajax behaviour in about section

--- backend/app/controllers/backend/about_sections_controller.rb
+++ backend/app/controllers/backend/about_sections_controller.rb
@@ -12,10 +12,18 @@ module Backend
     end
 
     def edit
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
 
     def new
       @about_section = AboutSection.new
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
 
     def update
