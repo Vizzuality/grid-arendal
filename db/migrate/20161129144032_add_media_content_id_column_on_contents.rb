d938b419a0a76113f1f9b946ea7781c6c205f79e
pjosh <jorgegar@gmail.com>
2016-11-29 15:59:48 +0100

    Refactor migrate name

--- db/migrate/20161129144032_add_media_content_id_column_on_contents.rb
+++ db/migrate/20161129144032_add_media_content_id_column_on_contents.rb
@@ -1,4 +1,4 @@
-class AddContentIdColumnOnContents < ActiveRecord::Migration[5.0]
+class AddMediaContentIdColumnOnContents < ActiveRecord::Migration[5.0]
   def change
     add_column :contents, :media_content_id, :integer
   end
