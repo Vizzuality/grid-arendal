a2bb53b7e44013eff5dfbff384f4271fd93ce2c2
Simao Belchior <simaobelchior@gmail.com>
2016-12-13 08:41:21 +0100

    fixes specs

--- spec/controllers/news_articles_controller_spec.rb
+++ spec/controllers/news_articles_controller_spec.rb
@@ -9,17 +9,4 @@ RSpec.describe NewsArticlesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET #show" do
-    before :each do
-      @news_article = create(:news_article)
-    end
-
-    it 'GET show returns http success' do
-      process :show, params: { id: @news_article.id }
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-  end
-
 end
