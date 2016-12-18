dc80009fec488e99c57e16570decfb52b57e0a7a
Simao Belchior <simaobelchior@gmail.com>
2016-12-18 18:50:42 +0000

    Enable filtering activities by tags on front end

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,6 +6,7 @@ class ActivitiesController < ApplicationController
     @activities = Activity.fetch_all(options_filter)
     @content_types = ContentType.by_activity
     @partners = Partner.order(:name)
+    @tags = Tag.order(:name)
     @section = SiteSection.where(section: "activities").first
     respond_to do |format|
       format.html
@@ -23,7 +24,7 @@ class ActivitiesController < ApplicationController
 
   private
     def options_filter
-      params.permit(:type, :partners, :years)
+      params.permit(:type, :partners, :years, :tags)
     end
     def set_activity
       @activity = Activity.find(params[:id])
