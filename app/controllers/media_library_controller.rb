d19f77726d673ccc393770e939e019e035ca8bc2
pjosh <jorgegar@gmail.com>
2017-01-03 15:40:01 +0100

    Using the same picture in the show landing as the media library index

--- app/controllers/media_library_controller.rb
+++ app/controllers/media_library_controller.rb
@@ -3,8 +3,9 @@ class MediaLibraryController < ApplicationController
   before_action :media_content, only: :show
 
   def index
-    @media_contents = MediaContent.wo_photos_in_album.
-      includes(:photo_sizes, :photos).order("publication_date DESC")
+    @media_contents = MediaContent.wo_photos_in_album
+                        .includes(:photo_sizes, :photos)
+                        .order("publication_date DESC")
     @tags = Tag.order(:name)
     @section = SiteSection.where(section: "media_library").first
   end
@@ -15,6 +16,8 @@ class MediaLibraryController < ApplicationController
   private
 
     def media_content
-      @media_content = MediaContent.find(params[:id])
+      @media_content = MediaContent.wo_photos_in_album
+                         .includes(:photo_sizes, :photos)
+                         .find(params[:id])
     end
 end
