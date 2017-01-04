ba6ab862681c2399e09db25ed21e1620c7aba80b
Simao Belchior <simaobelchior@gmail.com>
2017-01-04 11:59:33 +0000

    Remove picture from contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103124733) do
+ActiveRecord::Schema.define(version: 20170104114932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,10 +54,6 @@ ActiveRecord::Schema.define(version: 20170103124733) do
     t.string   "story_map_url"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.string   "picture_file_name"
-    t.string   "picture_content_type"
-    t.integer  "picture_file_size"
-    t.datetime "picture_updated_at"
     t.boolean  "is_featured"
     t.integer  "project_number"
     t.text     "short_description"
