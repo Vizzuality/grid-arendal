4a79961fa35f13686d5948df11bc52f1b4a7f5be
pjosh <jorgegar@gmail.com>
2017-01-04 11:50:13 +0100

    Refactor search limit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 class SearchController < ApplicationController
+  before_action :set_limit, only: [:index]
+
   def index
-    @news = NewsArticle.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").order(publication_date: :desc).limit(20)
-    @activities = Activity.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").limit(20)
-    @publications = Publication.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").limit(20)
-    @media_contents = MediaContent.wo_photos_in_album.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").limit(20)
+    @news = NewsArticle.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").order(publication_date: :desc).limit(@search_limit)
+    @activities = Activity.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").limit(@search_limit)
+    @publications = Publication.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").limit(@search_limit)
+    @media_contents = MediaContent.wo_photos_in_album.where("LOWER(title) like LOWER(?)", "%#{params[:query]}%").limit(@search_limit)
   end
 
+  def set_limit
+    @search_limit = 20
+  end
 end
