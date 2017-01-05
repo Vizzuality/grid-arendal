9b2c5e02c1db969d30aaa4715708cf7e684f35e3
pjosh <jorgegar@gmail.com>
2017-01-05 16:51:23 +0100

    * Pagination in news

--- app/assets/javascripts/controllers/news_controller.js
+++ app/assets/javascripts/controllers/news_controller.js
@@ -12,9 +12,30 @@
           el: '.masonry-layout'
         });
       }
+      this.scrollPaginationView = new App.View.ScrollPagination({
+        callback: this._paginate.bind(this)
+      });
     },
 
-    show: function(params) { },
+    _paginate: function() {
+      var params = _.extend({}, App.Helper.Utils.getGetParams(), { page: this.scrollPaginationView.page });
+      $.ajax({
+        method: "GET",
+        cache: true,
+        url: '/news/paginate',
+        data: params,
+        complete: function(response) {
+          this.scrollPaginationView.toggleDoingCallback();
+
+          if(response.status === 204) {
+            this.scrollPaginationView.toggleBlockPagination();
+          } else {
+            this.scrollPaginationView._setHash();
+          }
+
+        }.bind(this)
+      });
+    },
 
   });
 
--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,18 +1,33 @@
 # frozen_string_literal: true
 class NewsArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_news_limit, only: [:index, :paginate]
+  before_action :set_page_param, only: [:index, :paginate]
 
   def index
-    @articles = NewsArticle.order(publication_date: :desc)
+    @articles = NewsArticle
+                  .order(publication_date: :desc)
+                  .limit(@news_limit * @page)
     @section = SiteSection.where(section: "news").first
   end
 
-  def show
+  def paginate
+    @articles = NewsArticle
+                  .order(publication_date: :desc)
+                  .limit(@news_limit)
+                  .offset(@news_limit * (@page - 1))
+    respond_to do |format|
+      if(@articles.empty?)
+        head :no_content
+      end
+      format.js
+    end
   end
 
   private
-
-    def set_article
-      @article = NewsArticle.find(params[:id])
+    def set_page_param
+      @page = params[:page].present? ? params[:page].to_i : 1
+    end
+    def set_news_limit
+      @news_limit = 16
     end
 end
