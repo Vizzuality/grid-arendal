c6f7f0247e865e7b372a131890a001cf8832d070
edbrett <e.j.a.brett@gmail.com>
2017-01-09 18:48:10 +0100

    set chair to first position and then alphabetically by last name

--- app/controllers/about_sections_controller.rb
+++ app/controllers/about_sections_controller.rb
@@ -4,7 +4,7 @@ class AboutSectionsController < ApplicationController
     @sections  = AboutSection.order(:title)
     @vacancies = Vacancy.published
     @users = User.where(is_board_member: false).limit(6)
-    @board_members = User.where(is_board_member: true).order(:first_name, :last_name)
+    @board_members = User.where(is_board_member: true).order(:last_name)
     programme_tags = Tag.where(category: Tag::PROGRAMME)
     @programmes = Activity.where(title: programme_tags.map(&:name)).order(:title)
   end
