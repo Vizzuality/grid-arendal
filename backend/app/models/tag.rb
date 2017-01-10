229f8726657e41f64c403f6ca2a3cd4d5841e025
Simao Belchior <simaobelchior@gmail.com>
2017-01-10 16:00:02 +0000

    Improve tag fetching on front end

--- backend/app/models/tag.rb
+++ backend/app/models/tag.rb
@@ -1,4 +1,30 @@
 class Tag < ApplicationRecord
   PROGRAMME = "Programme"
   CATEGORIES = ["Activity Area", PROGRAMME, "Region", "Theme"]
+
+  def self.for_content content_type
+    query = <<-SQL
+      SELECT tags.id, tags.name, COUNT(*) AS taggings_count
+      FROM tags
+      INNER JOIN taggings ON taggings.tag_id = tags.id
+      INNER JOIN contents ON taggable_type = 'Content' AND
+        taggable_id = contents.id AND contents.type = '#{content_type}'
+      GROUP by tags.id, tags.name
+      ORDER BY UPPER(tags.name)
+    SQL
+    Tag.find_by_sql(query)
+  end
+
+  def self.for_media_content
+    query = <<-SQL
+      SELECT tags.id, tags.name, COUNT(*) AS taggings_count
+      FROM tags
+      INNER JOIN taggings ON taggings.tag_id = tags.id
+      INNER JOIN contents ON taggable_type = 'MediaContent' AND
+        taggable_id = contents.id
+      GROUP by tags.id, tags.name
+      ORDER BY UPPER(tags.name)
+    SQL
+    Tag.find_by_sql(query)
+  end
 end
