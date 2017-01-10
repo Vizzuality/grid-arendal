7c1e1b1b218b3d2bdcc3591ebfcfdcbf3761acb4
edbrett <e.j.a.brett@gmail.com>
2017-01-10 12:24:47 +0100

    merge dev

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require turbolinks
+// = require turbolinks
 //= require URIjs
 //= require handlebars
 //= require underscore/underscore
