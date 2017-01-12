fb30faba2bad24455abc180f19d280d6826f7781
edbrett <e.j.a.brett@gmail.com>
2017-01-12 14:34:11 +0100

    fix scroll based on position

--- app/assets/javascripts/views/scroll_to_top.js
+++ app/assets/javascripts/views/scroll_to_top.js
@@ -6,21 +6,56 @@
 
   App.View.ScrollToTop = Backbone.View.extend({
 
+    defaults: {
+      showPos: 1000
+    },
+
     el: 'body',
 
     events: {
-      'click .js-btn-scroll-top' : 'onClickScrollToTop',
+      'click .js-btn-scroll-top' : '_onClickScrollToTop',
     },
 
     initialize: function() {
       if (!this.el) {
         return;
       }
+      this._cache();
+      this._loadOnScrollEvent();
     },
 
-    onClickScrollToTop: function() {
+    _cache: function() {
+      this.$scroll = $('.c-scroll-to-top');
+    },
+
+    _onClickScrollToTop: function() {
       $("html, body").animate({ scrollTop: 0 }, "slow");
       return false;
+    },
+
+    _loadOnScrollEvent: function() {
+      $(window).on('scroll', function() {
+
+        var currentScroll = $(window).scrollTop();
+        var scrollBottom = $(document).height() - window.innerHeight - currentScroll;
+        
+        if ( currentScroll < this.defaults.showPos ) {
+          this.$scroll.css({
+            opacity: 0
+          });
+        } else if (currentScroll >= this.defaults.showPos && scrollBottom > 450 ) {
+          this.$scroll.css({
+            opacity: 1,
+            position: 'fixed'
+          });
+        } else {
+          this.$scroll.css({
+            position: 'absolute',
+            opacity: 1
+          });
+        }
+
+      }.bind(this));
     }
 
   });
