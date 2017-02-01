3f2c4f651ea3d02c146f1fd4966ee1f6b4be14c5
pjosh <jorgegar@gmail.com>
2017-02-01 16:03:18 +0100

    Activity tabs trackings

--- app/assets/javascripts/views/activities_anchors_view.js
+++ app/assets/javascripts/views/activities_anchors_view.js
@@ -19,7 +19,12 @@
     _onClickMoveScrollTo: function(e) {
       var section = $(e.currentTarget).data("section-belongs");
       this.scrollHandler.scrollTo($('[data-section="' + section + '"]'));
+      this._trackAnchor(section);
     },
+
+    _trackAnchor: function (section) {
+      ga('send', 'event', 'Activity Page', 'Click Tab', section);
+    }
 
   });
 
