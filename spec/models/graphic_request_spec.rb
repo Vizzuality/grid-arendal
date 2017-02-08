bc83a4994e6891c59070e899d09d8fe9f71ebd39
Simao Belchior <simaobelchior@gmail.com>
2017-02-08 13:24:42 +0000

    Fixes spec to respect validation rules

--- spec/models/graphic_request_spec.rb
+++ spec/models/graphic_request_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe GraphicRequest, type: :model do
   it 'Sets download_hash on create' do
-    graphic_req = GraphicRequest.new(email: "derp@derp.com")
+    graphic = Graphic.create
+    media_attachment = MediaAttachment.create
+    graphic_req = GraphicRequest.new(email: "derp@derp.com",
+                                     graphic_id: graphic.id,
+                                     media_attachment_id: media_attachment.id)
     graphic_req.save
     expect(graphic_req.download_hash).not_to be(nil)
   end
