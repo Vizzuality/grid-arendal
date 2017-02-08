101f8a2665ac1399d622770cdca5aab9f07e19ef
Simao Belchior <simaobelchior@gmail.com>
2017-02-08 12:01:27 +0000

    make sure to always set a media_attachment_id

--- backend/app/models/graphic_request.rb
+++ backend/app/models/graphic_request.rb
@@ -3,6 +3,8 @@ class GraphicRequest < ApplicationRecord
   before_create :generate_hash
   belongs_to :media_attachment
 
+  validates :media_attachment_id, presence: true
+
   private
 
   def generate_hash
