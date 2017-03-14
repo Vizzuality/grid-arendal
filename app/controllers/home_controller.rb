3111aae3bc713a5860cb4e75fe63bde758142be4
Simao Belchior <simaobelchior@gmail.com>
2017-03-14 22:54:49 +0100

    Randomize activities on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
   def index
     @users = User.with_category.regular_staff.randomize.limit(6)
     @news = NewsArticle.limit(4).order(publication_date: :desc)
-    @activities = Activity.featured.published.limit(3)
-    @publications = Publication.featured.published.order(content_date: :desc).limit(3)
+    @activities = Activity.featured.published.randomize.
+      limit(3).includes(:content_type, media_content: :photo_sizes)
+    @publications = Publication.featured.published.
+      order(content_date: :desc).limit(3)
     @event = Event.where(active: true).order(:created_at).limit(1)&.first
     @media_contents = MediaContent.albums_collections_and_videos.featured
     @section = SiteSection.where(section: "home").first
