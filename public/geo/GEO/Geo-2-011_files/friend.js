414f8befc7fb1f7792eb00e88075e5c798fc7e10
Simao Belchior <simaobelchior@gmail.com>
2017-03-28 11:02:01 +0100

    removing static pages

--- public/geo/GEO/Geo-2-011_files/friend.js
+++
@@ -1,9 +0,0 @@
-
-// Called by onLoad statement in the BODY tag.
-function openWindow()   {
-   var newWind = window.open('http://database.townhall.com/heritage/referemail.cfm?Referer=' + document.URL,'newWind','width=600,height=500');
-   }
-// De-activate Cloaking 
-//function specialWin() {
-	//var newwin = window.open('http://www.heritage.org/shorts/ssquickhitssample.html','newwin','height=500,width=450,scrollbars=yes');
-//}
--- public/geo1/script.js
+++
@@ -1,7 +0,0 @@
- //Check if window is in a frame or not	
-// if (window == top) 
-// {	
-	// var url = location.href.replace('www.','');
-	// url = url.replace('http://grida.no','');
-	// setTimeout('top.location.replace("/publications/other/geo1?src=" + url)', 100);
-// }
