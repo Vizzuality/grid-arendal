7e0fc5277af393b473dac9e75eaa6e92c8eb659c
Simao Belchior <simaobelchior@gmail.com>
2017-03-29 15:06:00 +0100

    Update a redirect for mastrec

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -12,6 +12,8 @@ class ErrorsController < ApplicationController
         notice: "You have been redirected to GRID Arendal's new website. If this is not the content you are looking for, please use our new search by clicking the magnifying glass on the right hand side."
     elsif user.present?
       redirect_to staff_path(user)
+    elsif split[0].downcase == "mastrec"
+      redirect_to "http://news.grida.no/mastrec"
     elsif ["climate", "geo", "aeo", "geo1", "geo3", "geo2000"].include?(split[0])
       redirect_to "http://old.grida.no/#{request.fullpath}"
     else
