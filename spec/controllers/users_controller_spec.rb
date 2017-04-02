7ca5d530c081d4a8002b9534bedd5d487ea8942b
Simao Belchior <simaobelchior@gmail.com>
2017-04-02 15:24:47 +0100

    Removes legacy spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET #show" do
     before :each do
       @user = create(:user)
